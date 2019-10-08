31bf5754bc03f2b543d77c138ac872e4e57a2af1
Markus Ylinen <markus.ylinen@siili.com>
2019-10-08 15:00:20 +0300

    Refactored localizedLayerInfo to own component, added OtherLanguagesPane component for reusability

--- bundles/admin/admin-layereditor/resources/locale/fi.js
+++ bundles/admin/admin-layereditor/resources/locale/fi.js
@@ -1,65 +1,65 @@
 Oskari.registerLocalization(
     {
-        "lang": "fi",
-        "key": "admin-layereditor",
-        "value": {
-            "editor-tool": "Muokkaa tasoa",
-            "flyout-title": "Karttatasohallinta",
-            "generalTabTitle": "Yleiset",
-            "visualizationTabTitle": "Visualisointi",
-            "additionalTabTitle": "Lisätiedot",
-            "permissionsTabTitle": "Oikeudet",
-            "interfaceAddress": "Rajapinnan osoite",
-            "username": "Käyttäjätunnus",
-            "password": "Salasana",
-            "usernameAndPassword": "Käyttäjätunnus ja salasana",
-            "uniqueName": "Karttatason yksilöivä nimi",
-            "generic": {
-                "placeholder": "Nimi kielellä {0}",
-                "descplaceholder": "Kuvaus kielellä {0}"
+        'lang': 'fi',
+        'key': 'admin-layereditor',
+        'value': {
+            'editor-tool': 'Muokkaa tasoa',
+            'flyout-title': 'Karttatasohallinta',
+            'generalTabTitle': 'Yleiset',
+            'visualizationTabTitle': 'Visualisointi',
+            'additionalTabTitle': 'Lisätiedot',
+            'permissionsTabTitle': 'Oikeudet',
+            'interfaceAddress': 'Rajapinnan osoite',
+            'username': 'Käyttäjätunnus',
+            'password': 'Salasana',
+            'usernameAndPassword': 'Käyttäjätunnus ja salasana',
+            'uniqueName': 'Karttatason yksilöivä nimi',
+            'generic': {
+                'placeholder': 'Nimi kielellä {0}',
+                'descplaceholder': 'Kuvaus kielellä {0}'
             },
-            "en": {
-                "placeholder": "Nimi englanniksi",
-                "descplaceholder": "Kuvaus englanniksi"
+            'en': {
+                'placeholder': 'Nimi englanniksi',
+                'descplaceholder': 'Kuvaus englanniksi'
             },
-            "fi": {
-                "placeholder": "Nimi suomeksi",
-                "descplaceholder": "Kuvaus suomeksi"
+            'fi': {
+                'placeholder': 'Nimi suomeksi',
+                'descplaceholder': 'Kuvaus suomeksi'
             },
-            "sv": {
-                "placeholder": "Nimi ruotsiksi",
-                "descplaceholder": "Kuvaus ruotsiksi"
+            'sv': {
+                'placeholder': 'Nimi ruotsiksi',
+                'descplaceholder': 'Kuvaus ruotsiksi'
             },
-            "dataProvider": "Tiedontuottaja",
-            "addDataProvider": "Lisää tiedontuottaja",
-            "mapLayerGroups": "Tason ryhmät",
-            "selectMapLayerGroupsButton": "Valitse ryhmät",
-            "cancel": "Peruuta",
-            "ok": "Ok",
-            "add": "Lisää",
-            "save": "Tallenna",
-            "delete": "Poista",
-            "opacity": "Peittävyys",
-            "style": "Oletustyyli",
-            "minScale": "Pienin mittakaava",
-            "maxScale": "Suurin mittakaava",
-            "minAndMaxScale": "Pienin ja suurin mittakaava",
-            "metaInfoIdDesc": "Metatiedon tiedostotunniste on XML-muotoisen metatietotiedoston tiedostotunniste. Se haetaan automaattisesti GetCapabilities-vastausviestistä.",
-            "metaInfoId": "Metatiedon tiedostotunniste",
-            "gfiContent": "Kohdetietoikkunan lisäsisältö",
-            "attributes": "Attribuutit",
-            "messages": {
-                "saveSuccess": "Tallennettu",
-                "saveFailed": "Järjestelmässä tapahtui virhe. Tietoja ei ole tallennettu.",
-                "confirmDeleteLayer": "Karttataso poistetaan. Haluatko jatkaa?",
-                "errorRemoveLayer": "Karttatason poisto ei onnistunut.",
-                "invalidStyleJson" : "Style JSON-syntaksi on virheellinen.",
-                "invalidHoverJson" : "Hover JSON-syntaksi on virheellinen.",
-                "errorInsertAllreadyExists": "Uusi karttataso on lisätty. Samalla tunnisteella on jo olemassa karttataso."
+            'dataProvider': 'Tiedontuottaja',
+            'addDataProvider': 'Lisää tiedontuottaja',
+            'mapLayerGroups': 'Tason ryhmät',
+            'selectMapLayerGroupsButton': 'Valitse ryhmät',
+            'cancel': 'Peruuta',
+            'ok': 'Ok',
+            'add': 'Lisää',
+            'save': 'Tallenna',
+            'delete': 'Poista',
+            'opacity': 'Peittävyys',
+            'style': 'Oletustyyli',
+            'minScale': 'Pienin mittakaava',
+            'maxScale': 'Suurin mittakaava',
+            'minAndMaxScale': 'Pienin ja suurin mittakaava',
+            'metaInfoIdDesc': 'Metatiedon tiedostotunniste on XML-muotoisen metatietotiedoston tiedostotunniste. Se haetaan automaattisesti GetCapabilities-vastausviestistä.',
+            'metaInfoId': 'Metatiedon tiedostotunniste',
+            'gfiContent': 'Kohdetietoikkunan lisäsisältö',
+            'attributes': 'Attribuutit',
+            'messages': {
+                'saveSuccess': 'Tallennettu',
+                'saveFailed': 'Järjestelmässä tapahtui virhe. Tietoja ei ole tallennettu.',
+                'confirmDeleteLayer': 'Karttataso poistetaan. Haluatko jatkaa?',
+                'errorRemoveLayer': 'Karttatason poisto ei onnistunut.',
+                'invalidStyleJson': 'Style JSON-syntaksi on virheellinen.',
+                'invalidHoverJson': 'Hover JSON-syntaksi on virheellinen.',
+                'errorInsertAllreadyExists': 'Uusi karttataso on lisätty. Samalla tunnisteella on jo olemassa karttataso.'
             },
-            "otherLanguages": "Muut kielet",
-            "styleJSON": "Style JSON",
-            "hoverJSON": "Hover JSON"
+            'otherLanguages': 'Muut kielet',
+            'styleJSON': 'Style JSON',
+            'hoverJSON': 'Hover JSON'
         }
     }
 );
